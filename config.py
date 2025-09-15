9955c69b03c1d45017873c1d500e6d530a1146c9
Gustavo Anjos <gustavosaeanjos@gmail.com>
2025-09-15 17:35:36 -0300

    arrumei o config

--- config.py
+++ config.py
@@ -7,11 +7,10 @@ class Config:
     # Configurações flask
     SECRET_KEY = os.getenv('SECRET_KEY')
 
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = ''
-    MYSQL_DB = 'tcc'
-    SECRET_KEY = 'senha_mucho_braba_123@$%&*'
+    MYSQL_HOST = os.getenv('MYSQL_HOST')
+    MYSQL_USER = os.getenv('MYSQL_USER')
+    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
+    MYSQL_DB = os.getenv('MYSQL_DB')
 
 
     # AWS S3
